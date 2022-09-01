e949be395ba4c6100177765c885f9072ed4b56aa
Evan Stokdyk <evan.stokdyk@gmail.com>
2022-09-01 14:06:38 -0700

    Adding all files w/ copied code

---
+++ src/Blob.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+
+public class Blob {
+	
+	
+	
+	//return byteArrayToHexString(md.digest(convertme))
+	
+
+
+	public static String byteArrayToHexString(byte[] b) {
+		String result = "";
+		for (int i=0; i < b.length; i++) {
+			result += Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+		}
+		return result;
+	}
+}
